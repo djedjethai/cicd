03e023f7c52d7a610612316f0705b9c9b29d4f80
Jerome Bidault <djedje.thai.ok@gmail.com>
2022-11-26 19:56:35 +0700

    okk

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -36,7 +36,7 @@ func (s *storage) Set(ctx context.Context, key string, value string) error {
 	_, sp := tr.Start(ctx, "SetStorage")
 	defer sp.End()
 
-	fmt.Println("is all good")
+	fmt.Println("is all good ahhhhhh")
 
 	// create node in dll
 	newN, outN := s.dll.unshift(key, value)
